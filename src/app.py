30e8736b16691ba97fc1bf47dfe364f9a63cc4bf
Thomas Dinh <th_dinh@yahoo.fr>
2026-02-05 18:08:48 +0100

    Streamlit Commit 2

--- src/app.py
+++ src/app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pathlib import Path
 
 co2_path = "data/CO2_per_capita.csv"
 co2_df = pd.read_csv(co2_path, sep=";")
